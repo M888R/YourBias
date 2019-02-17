53b10be4ac49ff5d49f9fbdf2038a8f130630c81
Manas Shah <manas@shahparivar.info>
2019-02-16 16:58:06 -0800

    almost finished score counting, needs more tuning

--- scan.js
+++ scan.js
@@ -1,11 +1,12 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({score: 0}, function() {
+    chrome.storage.sync.set({userScore: 0}, function() {
         console.log("Bias is Neutral by default.");
     });
 
-    chrome.storage.sync.set({sites: JSON.parse(sources_database)}, function() {
-        console.log("Stored site database.");
-    });
+    // var sites_database = JSON.parse("./sources_database.json");
+    // chrome.storage.sync.set({sites: sites_database}, function() {
+    //     console.log("Stored site database.");
+    // });
 });
 
 chrome.history.onVisited.addListener(function (result) {
@@ -14,36 +15,47 @@ chrome.history.onVisited.addListener(function (result) {
     var sites = [];
     
     var sections = currentSiteUrl.split(".");
-    var reachedWWW = false;
-    var reachedCom = false;
-    for (var i = 0; i < sections.length - 1; i++) {
-        if (sections[i + 1].includes("com")) {
-            reachedCom = true;
-            break;
-        }
-        if (reachedWWW) {
-            currentSite += sections[i + 1];
-        }
-        if (sections[i].includes("www")) {
-            currentSite += sections[i + 1];
-            reachedWWW = true;
-        }
-    }
-    if (reachedCom) {
-        chrome.storage.sync.get('sites', function(sitewrapper) {
-            sites = sitewrapper.sites;
-        });
+    // var reachedWWW = false;
+    // var reachedCom = false;
+    // for (var i = 0; i < sections.length - 1; i++) {
+    //     if (sections[i + 1].includes("com")) {
+    //         reachedCom = true;
+    //         break;
+    //     }
+    //     if (reachedWWW) {
+    //         currentSite += sections[i + 1];
+    //     }
+    //     if (sections[i].includes("www")) {
+    //         currentSite += sections[i + 1];
+    //         reachedWWW = true;
+    //     }
+    // }
+    currentSite = sections[1];
+    console.log("Opened site " + currentSite);
+    //if (reachedCom) {
+        // chrome.storage.sync.get('sites', function(sitewrapper) {
+        //     sites = sitewrapper.sites;
+        // });
+        var request = new XMLHttpRequest();
+        request.open("GET", "./sources_database.json", false);
+        request.send(null)
+        var sites = JSON.parse(request.responseText);
+
         for (var i = 0; i < sites.length; i++) {
             let currentCheckUrl = sites[i].url;
             if (currentCheckUrl.includes(currentSite)) {
                 var oldScore = 0;
-                chrome.storage.sync.get('score', function (result) {
-                    oldScore = result.score;
+                chrome.storage.sync.get('userScore', function (result) {
+                    oldScore = result.userScore;
+                    console.log("Old Score: " + oldScore);
                 });
-                chrome.storage.sync.set({score: oldScore + sites[i].score}, function() {
-                    console.log("Updated score to " + oldScore + sites[i].score);
+                console.log("site score: " + sites[i].score);
+                console.log("sum: " + (oldScore + sites[i].score));
+                var newScore = (oldScore * 0.3) + (sites[i].score * 0.7);
+                chrome.storage.sync.set({'userScore': newScore}, function() {
+                    console.log("Updated score to " + newScore);
                 });
             }
         }
-    }
+    //}
 });
