9a39a58775d969c35fcf8cf354a8a5859ac1a280
Manas Shah <manas@shahparivar.info>
2019-02-17 09:17:37 -0800

    company recs working

--- companies.js
+++ companies.js
@@ -1,10 +1,43 @@
 async function loadCompanies() {
-    const score = await chromeStorageGetPromise('userScore');
-    if (score > 70) {
+    const scoreWrapper = await chromeStorageGetPromise('userScore');
+    const score = scoreWrapper.userScore;
+    console.log(score);
+    var supportCompanies = [];
+    var againstCompanies = [];
 
-    } else if (score > 30) {
+    var request = new XMLHttpRequest();
+    request.open("GET", "./companies_database.json", false);
+    request.send(null)
+    var companies = JSON.parse(request.responseText);
+    console.log(companies.extremeconservative);
 
-    } else if (score)
+    if (score > 50) { // extreme conservative
+        supportCompanies = companies.extremeconservative;
+        console.log(supportCompanies);
+        againstCompanies = companies.extremeliberal;
+    } else if (score > 0) { // somewhat conservative
+        supportCompanies = companies.somewhatconservative;
+        againstCompanies = companies.somewhatliberal;
+    } else if (score > -50) { // somewhat liberal
+        supportCompanies = companies.somewhatliberal;
+        againstCompanies = companies.somewhatconservative;
+    } else if (score < -50) { // extreme liberal
+        supportCompanies = companies.extremeliberal;
+        againstCompanies = companies.extremeconservative;
+    }
+
+    for (var i = 0; i < supportCompanies.length; i++) {
+        var currentListing = document.createElement("LI");
+        var company = document.createTextNode(supportCompanies[i]);
+        currentListing.appendChild(company);
+        document.getElementById("supportlist").appendChild(currentListing);
+    }
+    for (var i = 0; i < againstCompanies.length; i++) {
+        var currentListing = document.createElement("LI");
+        var company = document.createTextNode(againstCompanies[i]);
+        currentListing.appendChild(company);
+        document.getElementById("avoidlist").appendChild(currentListing);
+    }
 }
 
 function chromeStorageGetPromise (...args) {
